7e553b1fba5e6eeec5d25b95c510859eec7a948a
Nameetha <namitha458193-raj>
2026-08-13 20:20:20 +0530

    first commit

--- BankingManagement2.java
+++ BankingManagement2.java
@@ -51,7 +51,8 @@ public class BankingManagement2 {
             System.out.println("3. Withdraw");
             System.out.println("4. Check Balance");
             System.out.println("5. Exit");
-
+            System.out.println("6.View All details");
+            System.out.print("Enter your choice");
             int choice = sc.nextInt();
 
             switch (choice) {
@@ -59,6 +60,11 @@ public class BankingManagement2 {
                 case 1:
                     System.out.print("Enter Account Number: ");
                     int accNo = sc.nextInt();
+                    if(accounts.containsKey(accNo)){
+                        System.out.println("Account Number is already exits");
+                        break;
+                    }
+
                     sc.nextLine();
                     System.out.print("Enter Account Holder Name: ");
                     String name = sc.nextLine();
@@ -127,6 +133,19 @@ public class BankingManagement2 {
                     System.out.println("Thank You!");
                     sc.close();
                     System.exit(0);
+                case 6:
+                     System.out.println("\n===== All Account Details =====");
+
+                    if (accounts.isEmpty()) {
+                        System.out.println("No accounts available.");
+                    } else {
+                        for (BankAccount account : accounts.values()) {
+                            System.out.println("Account Number : " + account.getAccountNumber());
+                            System.out.println("Account Holder : " + account.getAccountHolderName());
+                            System.out.println("Balance : " + account.getBalance());
+                        }
+                    }
+                    break;
                 default:
                     System.out.println("Invalid Choice!");
             }
