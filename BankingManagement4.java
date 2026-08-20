ed82e51c4d6b682148bc6ed434338df07c4912a4
Nameetha <namitha458193-raj>
2026-08-20 21:16:16 +0530

    case3 and case 4

--- BankingManagement4.java
+++ BankingManagement4.java
@@ -32,7 +32,9 @@ public class BankingManagement4 {
             System.out.println("\n===== BANKING MANAGEMENT SYSTEM =====");
             System.out.println("1. Create Account");
             System.out.println("2. Deposit");
-            System.out.println("3. Exit");
+            System.out.println("3.withdraw");
+            System.out.println("4. Display Account");
+            System.out.println("5. Exit");
 
             System.out.print("Enter your choice: ");
             int choice = sc.nextInt();
@@ -89,8 +91,74 @@ public class BankingManagement4 {
                     System.out.println("Current Balance: "
                             + acc.balance);
                     break;
+                                case 3:
 
-                case 3:
+                    System.out.print("Enter Account Number: ");
+                    accNo = sc.nextInt();
+
+                    acc = accounts.get(accNo);
+
+                    if (acc == null) {
+
+                        System.out.println(
+                                "Account not found!");
+
+                        break;
+                    }
+
+                    System.out.print("Enter Withdraw Amount: ");
+                    amount = sc.nextDouble();
+
+                    if (amount <= 0) {
+
+                        System.out.println(
+                                "Invalid withdrawal amount!");
+
+                        break;
+                    }
+
+                    if (amount > acc.balance) {
+
+                        System.out.println(
+                                "Insufficient balance!");
+
+                        break;
+                    }
+
+                    acc.balance = acc.balance - amount;
+
+                    System.out.println(
+                            "Withdrawal successful!");
+
+                    System.out.println(
+                            "Current Balance: "
+                            + acc.balance);
+
+                    break;
+
+                case 4:
+
+                    System.out.print("Enter Account Number: ");
+                    accNo = sc.nextInt();
+
+                    acc = accounts.get(accNo);
+
+                    if (acc == null) {
+
+                        System.out.println(
+                                "Account not found!");
+
+                    } else {
+
+                        System.out.println(
+                                "\n----- ACCOUNT DETAILS -----");
+
+                        acc.display();
+                    }
+
+                    break;
+
+                case 5:
                     System.out.println("Thank you for using the Banking System!");
                     sc.close();
                     return;
